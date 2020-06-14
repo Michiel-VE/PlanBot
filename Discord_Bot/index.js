86ca3bd369f30a31fb9d88072c314f8cec129f1f
Michiel-VE <r0794174@student.thomasmore.be>
2020-06-14 14:48:05 +0200

    update token

--- Discord_Bot/index.js
+++ Discord_Bot/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const Bot = new Discord.Client();
 
-const config = require('config.json');
+const config = require('./config.json');
 const prefix = '!p';
 
 var events = [];
